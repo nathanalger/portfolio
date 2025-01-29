af5cbacafe5e5d9e29d671a7dd695841b4f86db5
Nathan Alger <nalger@mtu.edu>
2025-01-28 20:37:50 -0500

    cleanup: refactoring file structure and routing

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,11 +1,12 @@
-import { createBrowserRouter, Outlet } from "react-router-dom";
-import Home from "./pages/home";
-import Projects from "./pages/Projects";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Projects from "./components/pages/Projects";
+import Root from "./components/pages/Root";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
-    element: <Outlet />,
+    element: <Root />,
     children: [
       {
         path: "/",
